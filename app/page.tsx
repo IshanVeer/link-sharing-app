4c1e8929d9c418044c2ad4e9ae645de49cbe2558
Ishan Veer <ishanpveer@gmail.com>
2025-08-07 21:23:53 +0530

    added utility classes according to style sheet

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div>
-      <h2 className="text-3xl font-bold text-red-500">hello world</h2>
+      <h2 className="hm-bold font-bold text-primary-500">hello world</h2>
+      <h2 className="hs-bold font-bold text-primary-300">hello world</h2>
+      <h2 className="body-m font-bold text-primary-200">hello world</h2>
+      <h2 className="body-s font-bold text-dark-100">hello world</h2>
     </div>
   );
 }
