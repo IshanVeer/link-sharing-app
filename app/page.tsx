8efdb30c61f8143198dc495d1bf53e676d74a0d4
Ishan Veer <ishanpveer@gmail.com>
2025-08-09 13:58:04 +0530

    added assets

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@ export default function Home() {
   return (
     <div>
       <h2 className="hm-bold font-bold text-primary-500">hello world</h2>
-      <h2 className="hs-bold font-bold text-primary-300">hello world</h2>
-      <h2 className="body-m font-bold text-primary-200">hello world</h2>
-      <h2 className="body-s font-bold text-dark-100">hello world</h2>
     </div>
   );
 }
